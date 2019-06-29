b3fb446936e6a7a5e58d0c607c9e343bbc6394b6
Scott Pierce <scott.pierce@rallyhealth.com>
2019-06-29 14:39:45 -0400

    save

--- html-builder/src/commonMain/kotlin/dev/scottpierce/html/style/Style.kt
+++ html-builder/src/commonMain/kotlin/dev/scottpierce/html/style/Style.kt
@@ -7,24 +7,7 @@ import dev.scottpierce.html.write.Writable
 import kotlin.Unit
 
 interface Style : Writable {
-    val size: Int
-
-    operator fun get(property: String): Any?
-    operator fun iterator(): Iterator<Map.Entry<String, Any?>>
-
-    operator fun plus(style: Style): Style {
-        val newProperties = LinkedHashMap<String, Any?>(size + style.size)
-
-        for (prop in this) {
-            newProperties[prop.key] = prop.value
-        }
-
-        for (prop in style) {
-            newProperties[prop.key] = prop.value
-        }
-
-        return StyleBuilder(newProperties)
-    }
+    val properties: MutableMap<String, Any?>
 
     override fun write(writer: HtmlWriter) {
         writer.writeStyle(this)
@@ -32,34 +15,33 @@ interface Style : Writable {
 }
 
 class StyleBuilder(
-    internal val properties: MutableMap<String, Any?> = LinkedHashMap()
+    override val properties: MutableMap<String, Any?> = LinkedHashMap()
 ) : Style {
-    override val size: Int
-        get() = properties.size
-
-    override fun get(property: String): Any? = properties[property]
-
-    override fun iterator(): Iterator<Map.Entry<String, Any?>> = properties.iterator()
-
     operator fun set(property: String, value: Any?) {
         properties[property] = value
     }
 
-    override operator fun plus(style: Style): StyleBuilder {
-        val properties = LinkedHashMap(properties).apply {
-//            putAll(style.properties)
-        }
-        return StyleBuilder(properties)
-    }
-
     operator fun Style.unaryPlus() {
-    }
-
-    operator fun StyleBuilder.unaryPlus() {
         properties.putAll(this.properties)
     }
 }
 
 inline fun style(func: StyleBuilder.() -> Unit): Style {
     return StyleBuilder().apply(func)
+}
+
+operator fun Style.get(property: String): Any? = properties[property]
+
+operator fun Style.plus(style: Style): Style {
+    val newProperties = LinkedHashMap<String, Any?>(properties.size + style.properties.size)
+
+    for (prop in properties) {
+        newProperties[prop.key] = prop.value
+    }
+
+    for (prop in style.properties) {
+        newProperties[prop.key] = prop.value
+    }
+
+    return StyleBuilder(newProperties)
 }
--- html-builder/src/commonMain/kotlin/dev/scottpierce/html/style/StyleUtil.kt
+++ html-builder/src/commonMain/kotlin/dev/scottpierce/html/style/StyleUtil.kt
@@ -3,5 +3,16 @@ package dev.scottpierce.html.style
 import dev.scottpierce.html.write.HtmlWriter
 
 fun HtmlWriter.writeStyle(style: Style) {
+    for ((property, value) in style.properties) {
+        if (value != null) {
+            write(property).write(':')
 
+            if (!options.minifyStyles) {
+                write(' ')
+            }
+
+            write(value.toString())
+            write(';')
+        }
+    }
 }
