a8b8256ddf0e9826f59c92e1ed1e8073a5642ba3
Scott Pierce <scott.pierce@rallyhealth.com>
2019-06-29 23:29:48 -0400

    generate tests for void elements

--- html-builder-generator/src/main/kotlin/dev/scottpierce/html/generate/GenerateElementTests.kt
+++ html-builder-generator/src/main/kotlin/dev/scottpierce/html/generate/GenerateElementTests.kt
@@ -9,12 +9,12 @@ fun generateElementTests() {
 
     val normalTemplate = TEMPLATE_NORMAL_FILE.bufferedReader().readText()
 
-    for (i in Element.values.indices) {
+    elementLoop@ for (i in Element.values.indices) {
         val element = Element.values[i]
 
         // These are the template tests
-        if (element.tagName == "div") {
-            continue
+        when (element.tagName) {
+            "div", "br" -> continue@elementLoop
         }
 
         val capitalizedTag = element.tagName.capitalize()
@@ -24,6 +24,20 @@ fun generateElementTests() {
                 val testString = normalTemplate
                     .replace("Div", capitalizedTag)
                     .replace("div", element.tagName)
+                    .replace("BodyContext", element.callingContext.contextClassName.simpleName)
+
+                File("html-builder/src/genTest/kotlin/dev/scottpierce/html/element/${capitalizedTag}Test.kt").apply {
+                    parentFile.mkdirs()
+                    createNewFile()
+                }.bufferedWriter().use {
+                    it.write(testString)
+                }
+            }
+
+            is Element.Void -> {
+                val testString = normalTemplate
+                    .replace("Br", capitalizedTag)
+                    .replace("br", element.tagName)
                     .replace("BodyContext", element.callingContext.contextClassName.simpleName)
 
                 File("html-builder/src/genTest/kotlin/dev/scottpierce/html/element/${capitalizedTag}Test.kt").apply {
