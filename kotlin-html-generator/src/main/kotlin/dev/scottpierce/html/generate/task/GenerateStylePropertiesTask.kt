d2fc7c948c5fd99bed4d62e7e5b272c09b07d765
Scott Pierce <scott.pierce@rallyhealth.com>
2019-11-03 22:37:55 -0500

    no more style tests

--- kotlin-html-generator/src/main/kotlin/dev/scottpierce/html/generate/task/GenerateStylePropertiesTask.kt
+++ kotlin-html-generator/src/main/kotlin/dev/scottpierce/html/generate/task/GenerateStylePropertiesTask.kt
@@ -1,31 +1,25 @@
 package dev.scottpierce.html.generate.task
 
 import com.squareup.kotlinpoet.AnnotationSpec
-import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.MemberName
 import com.squareup.kotlinpoet.PropertySpec
-import com.squareup.kotlinpoet.TypeSpec
 import dev.scottpierce.html.generate.Task
-import dev.scottpierce.html.generate.model.ASSERT_EQUALS
 import dev.scottpierce.html.generate.model.BASE_STYLE_CONTEXT
 import dev.scottpierce.html.generate.model.Constants
-import dev.scottpierce.html.generate.model.PropertyType
-import dev.scottpierce.html.generate.model.STYLE_MEMBER
 import dev.scottpierce.html.generate.model.StyleProperty
-import dev.scottpierce.html.generate.model.TEST
 import dev.scottpierce.html.generate.model.UNSUPPORTED_OPERATION_EXCEPTION
-import java.io.File
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
+import java.io.File
 
 class GenerateStylePropertiesTask : Task {
     companion object {
-        private val STYLE_PROPERTIES_FILE = File("${Constants.BASE_GEN_DIR}/dev/scotpierce/html/style")
-        private val STYLE_PROPERTY_TESTS_FILE = File("${Constants.BASE_GEN_TEST_DIR}/dev/scotpierce/html/style")
+        private val STYLE_PROPERTIES_FILE = File("${Constants.BASE_GEN_DIR}/dev/scotpierce/html/writer/style")
+        private val STYLE_PROPERTY_TESTS_FILE = File("${Constants.BASE_GEN_TEST_DIR}/dev/scotpierce/html/writer/style")
     }
 
     override val name: String = "Generate Style Properties"
@@ -43,30 +37,30 @@ class GenerateStylePropertiesTask : Task {
         val propertiesFile = FileSpec.builder(Constants.STYLE_PACKAGE, "StyleProperties")
             .indent("    ")
 
-        val propertyTestsFile = FileSpec.builder(Constants.STYLE_PACKAGE, "StylePropertyTests")
-            .indent("    ")
+        // val propertyTestsFile = FileSpec.builder(Constants.STYLE_PACKAGE, "StylePropertyTests")
+        //     .indent("    ")
 
         propertiesFile.addComment(Constants.GENERATED_FILE_COMMENT)
-        propertyTestsFile.addComment(Constants.GENERATED_FILE_COMMENT)
+        // propertyTestsFile.addComment(Constants.GENERATED_FILE_COMMENT)
 
-        val testClass: TypeSpec.Builder = TypeSpec.classBuilder("StylePropertyTests")
+        // val testClass: TypeSpec.Builder = TypeSpec.classBuilder("StylePropertyTests")
 
         for (property in StyleProperty.values()) {
             generateProperty(propertiesFile, property)
-            generatePropertyTests(testClass, property)
+            // generatePropertyTests(testClass, property)
         }
 
-        propertyTestsFile.addType(testClass.build())
+        // propertyTestsFile.addType(testClass.build())
 
         listOf(
             GlobalScope.launch(Dispatchers.IO) {
                 propertiesFile.build()
                     .writeTo(Constants.BASE_GEN_DIR)
-            },
-            GlobalScope.launch(Dispatchers.IO) {
-                propertyTestsFile.build()
-                    .writeTo(Constants.BASE_GEN_TEST_DIR)
             }
+            // GlobalScope.launch(Dispatchers.IO) {
+            //     propertyTestsFile.build()
+            //         .writeTo(Constants.BASE_GEN_TEST_DIR)
+            // }
         ).joinAll()
     }
 
@@ -107,42 +101,42 @@ class GenerateStylePropertiesTask : Task {
         )
     }
 
-    private fun generatePropertyTests(testClass: TypeSpec.Builder, property: StyleProperty) {
-        testClass.addFunction(
-            FunSpec.builder(property.propertyName).apply {
-                addAnnotation(TEST)
-
-                val propertyMember = MemberName("dev.scottpierce.html.writer.style", property.propertyName)
-
-                val expected: CodeBlock = when (property.type) {
-                    PropertyType.COLOR -> CodeBlock.of("color(\"#555555\")")
-                    PropertyType.DISPLAY -> CodeBlock.of("Display.BLOCK")
-                    PropertyType.DIMENSION -> CodeBlock.of("Dimension.Size(1f, DimensionUnit.PX)")
-                    PropertyType.ALIGN_ITEMS -> CodeBlock.of("AlignItems.BASELINE")
-                    PropertyType.FLEX_DIRECTION -> CodeBlock.of("FlexDirection.COLUMN")
-                    PropertyType.JUSTIFY_CONTENT -> CodeBlock.of("JustifyContent.CENTER")
-                    PropertyType.INT -> CodeBlock.of("100")
-                    PropertyType.FLOAT -> CodeBlock.of("100f")
-                    PropertyType.DOUBLE -> CodeBlock.of("100.0")
-                    PropertyType.STRING -> CodeBlock.of("\"boo\"")
-                    PropertyType.POSITION -> CodeBlock.of("Position.STATIC")
-                    PropertyType.LIST_STYLE -> CodeBlock.of("ListStyle(ListStyle.Type.DISC)")
-                    PropertyType.FLOAT_DIRECTION -> CodeBlock.of("FloatDirection.LEFT")
-                    PropertyType.TEXT_TRANSFORM -> CodeBlock.of("TextTransform.NONE")
-                    PropertyType.ALIGN_SELF -> CodeBlock.of("AlignSelf.AUTO")
-                    PropertyType.TEXT_ALIGN -> CodeBlock.of("TextAlign.CENTER")
-                    PropertyType.JUSTIFY_SELF -> CodeBlock.of("JustifySelf.CENTER")
-                    PropertyType.FLEX_WRAP -> CodeBlock.of("FlexWrap.NO_WRAP")
-                    PropertyType.VISIBILITY -> CodeBlock.of("Visibility.VISIBLE")
-                }
-
-                beginControlFlow("val style = %M", STYLE_MEMBER)
-                addCode("%M = ", propertyMember).addCode(expected).addCode("\n")
-                addCode("%M(", ASSERT_EQUALS).addCode(expected).addCode(", %M)", propertyMember).addCode("\n")
-                endControlFlow()
-
-                addCode("%M(", ASSERT_EQUALS).addCode(expected).addCode(", style.%M)", propertyMember).addCode("\n")
-            }.build()
-        )
-    }
+//    private fun generatePropertyTests(testClass: TypeSpec.Builder, property: StyleProperty) {
+//        testClass.addFunction(
+//            FunSpec.builder(property.propertyName).apply {
+//                addAnnotation(TEST)
+//
+//                val propertyMember = MemberName("dev.scottpierce.html.writer.style", property.propertyName)
+//
+//                val expected: CodeBlock = when (property.type) {
+//                    PropertyType.COLOR -> CodeBlock.of("color(\"#555555\")")
+//                    PropertyType.DISPLAY -> CodeBlock.of("Display.BLOCK")
+//                    PropertyType.DIMENSION -> CodeBlock.of("Dimension.Size(1f, DimensionUnit.PX)")
+//                    PropertyType.ALIGN_ITEMS -> CodeBlock.of("AlignItems.BASELINE")
+//                    PropertyType.FLEX_DIRECTION -> CodeBlock.of("FlexDirection.COLUMN")
+//                    PropertyType.JUSTIFY_CONTENT -> CodeBlock.of("JustifyContent.CENTER")
+//                    PropertyType.INT -> CodeBlock.of("100")
+//                    PropertyType.FLOAT -> CodeBlock.of("100f")
+//                    PropertyType.DOUBLE -> CodeBlock.of("100.0")
+//                    PropertyType.STRING -> CodeBlock.of("\"boo\"")
+//                    PropertyType.POSITION -> CodeBlock.of("Position.STATIC")
+//                    PropertyType.LIST_STYLE -> CodeBlock.of("ListStyle(ListStyle.Type.DISC)")
+//                    PropertyType.FLOAT_DIRECTION -> CodeBlock.of("FloatDirection.LEFT")
+//                    PropertyType.TEXT_TRANSFORM -> CodeBlock.of("TextTransform.NONE")
+//                    PropertyType.ALIGN_SELF -> CodeBlock.of("AlignSelf.AUTO")
+//                    PropertyType.TEXT_ALIGN -> CodeBlock.of("TextAlign.CENTER")
+//                    PropertyType.JUSTIFY_SELF -> CodeBlock.of("JustifySelf.CENTER")
+//                    PropertyType.FLEX_WRAP -> CodeBlock.of("FlexWrap.NO_WRAP")
+//                    PropertyType.VISIBILITY -> CodeBlock.of("Visibility.VISIBLE")
+//                }
+//
+//                beginControlFlow("val style = %M", STYLE_MEMBER)
+//                addCode("%M = ", propertyMember).addCode(expected).addCode("\n")
+//                addCode("%M(", ASSERT_EQUALS).addCode(expected).addCode(", %M)", propertyMember).addCode("\n")
+//                endControlFlow()
+//
+//                addCode("%M(", ASSERT_EQUALS).addCode(expected).addCode(", style.%M)", propertyMember).addCode("\n")
+//            }.build()
+//        )
+//    }
 }
--- kotlin-html-writer/src/genMain/kotlin/dev/scottpierce/html/writer/element/Bold.kt
+++ kotlin-html-writer/src/genMain/kotlin/dev/scottpierce/html/writer/element/Bold.kt
@@ -47,10 +47,8 @@ inline fun BodyContext.bold(func: BodyContext.() -> Unit = {}) {
 }
 
 @HtmlDsl
-inline fun BodyContext.bold(
-    vararg attrs: Pair<String, String?>,
-    func: BodyContext.() -> Unit = {}
-) {
+inline fun BodyContext.bold(vararg attrs: Pair<String, String?>, func: BodyContext.() -> Unit =
+        {}) {
     writer.writeTag("bold")
     writer.writeAttributes(attrs)
     writer.write('>')
