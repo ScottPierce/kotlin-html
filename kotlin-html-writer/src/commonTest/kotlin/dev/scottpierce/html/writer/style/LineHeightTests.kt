687226c7ba6a11b88c35f27050ee6660e65a846f
Scott Pierce <scott.pierce@rallyhealth.com>
2019-11-04 21:34:43 -0500

    add more line height tests

--- kotlin-html-writer/src/commonTest/kotlin/dev/scottpierce/html/writer/style/LineHeightTests.kt
+++ kotlin-html-writer/src/commonTest/kotlin/dev/scottpierce/html/writer/style/LineHeightTests.kt
@@ -1,5 +1,6 @@
 package dev.scottpierce.html.writer.style
 
+import dev.scottpierce.html.writer.WriteOptions
 import dev.scottpierce.html.writer.util.assertEquals
 import dev.scottpierce.html.writer.util.writeStyle
 import kotlin.test.Test
@@ -20,6 +21,33 @@ class LineHeightTests {
             lineHeight = 2.px
         } assertEquals {
             """line-height: 2px;"""
+        }
+    }
+
+    @Test
+    fun inherit() {
+        writeStyle {
+            lineHeight = LineHeight.INHERIT
+        } assertEquals {
+            """line-height: inherit;"""
+        }
+    }
+
+    @Test
+    fun normal() {
+        writeStyle {
+            lineHeight = LineHeight.NORMAL
+        } assertEquals {
+            """line-height: normal;"""
+        }
+    }
+
+    @Test
+    fun minimal() {
+        writeStyle(WriteOptions.minified) {
+            lineHeight = 2.px
+        } assertEquals {
+            """line-height:2px;"""
         }
     }
 }
