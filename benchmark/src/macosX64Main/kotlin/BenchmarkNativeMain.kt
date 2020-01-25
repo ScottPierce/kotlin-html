8c70c8e83685c1841b6129992d8ada5f045f4d77
Scott Pierce <scott.pierce@rallyhealth.com>
2020-01-24 23:49:52 -0500

    save

--- benchmark/src/macosX64Main/kotlin/BenchmarkNativeMain.kt
+++ benchmark/src/macosX64Main/kotlin/BenchmarkNativeMain.kt
@@ -1,30 +1,49 @@
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
+import kotlin.native.concurrent.isFrozen
+import kotlin.random.Random
 
 fun main() {
-    println("Starting")
+    val context1: CoroutineContext = newSingleThreadContext("Context1")
 
-    var dataUsedInCoroutine = MutableData(1)
+    runBlocking(context1) {
+        println("Example 1")
 
-    val job1 = GlobalScope.launch {
-        dataUsedInCoroutine = MutableData(2)
+        val example1 = Example1(context1)
+
+        example1.example1()
+        example1.example1()
+        example1.example1()
+
+        println("Data Value: ${example1.data.value}")
+        println("Is MutableData Frozen : ${example1.data.isFrozen}")
+        println("Is Example1 Frozen: ${example1.isFrozen}")
     }
+}
 
-    println("Job Created")
+object Background {
+    val context: CoroutineContext = newSingleThreadContext("Background") + NonCancellable
 
-    runBlocking {
-        job1.join()
+    suspend fun asyncRandomInt(): Int = withContext(context) {
+        5
     }
-
-    newSingleThreadContext("")
-
-    println("Joined")
-    println(dataUsedInCoroutine.value)
-    println("Finished")
 }
 
 data class MutableData(
     var value: Int
 )
+
+class Example1(val context: CoroutineContext) {
+    val data = MutableData(0)
+
+    suspend fun example1() = withContext(context) {
+        val randomValue = Background.asyncRandomInt()
+        data.value += randomValue
+    }
+}
+
